2591f5722540570b9dad313411ac377de0fecebd
nmarc40290-glitch <nmarc64270@gmail.com>
2026-03-29 17:10:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,10 @@
-const CACHE_NAME = 'potager-v2';
-const ASSETS = [
-  'index.html',
-  'manifest.json'
-];
+const CACHE_NAME = 'potager-cache-v1';
+const urlsToCache = ['index.html', 'manifest.json'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
