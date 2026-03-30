e9bcc4113696ec262747c8249f06943d4bb9c3ed
nmarc40290-glitch <nmarc64270@gmail.com>
2026-03-30 20:41:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,10 @@
-const CACHE_NAME = 'potager-pwa-cache-v1';
-const ASSETS = [
-  'index.html',
-  'style.css',
-  'script.js',
-  'manifest.json'
-];
+const CACHE_NAME = 'potager-cache-v1';
+const urlsToCache = ['index.html', 'manifest.json'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
